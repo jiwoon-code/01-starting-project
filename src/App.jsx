8ae10490a931ec99c8db822fd5e9080eca76f142
woon <115874237+jiwoon-code@users.noreply.github.com>
2025-04-02 06:10:30 +0000

    git commit --amend

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,47 @@
+import { useState } from "react";
 import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept";
 import TabButton from "./components/TabButton";
 
 function App() {
-    let tabContent = "Please click a button"
-    function handleSelect(selectedButton) {
-       tabContent = selectedButton
-    }
+  let tabContent = "Please click a button";
+  function handleSelect(selectedButton) {
+    tabContent = selectedButton;
+  }
 
-    return (
-        <div>
-            <Header />
-            <main>
-                <section id="core-concepts">
-                    <h2>Core Concepts</h2>
-                    <ul>
-                        <CoreConcept
-                            title={CORE_CONCEPTS[0].title}
-                            description={CORE_CONCEPTS[0].description}
-                            image={CORE_CONCEPTS[0].image}
-                        />
-                        <CoreConcept {...CORE_CONCEPTS[1]} />
-                        <CoreConcept {...CORE_CONCEPTS[2]} />
-                        <CoreConcept {...CORE_CONCEPTS[3]} />
-                    </ul>
-                </section>
-                <section id="examples">
-                    <h2>Examples</h2>
-                    <menu>
-                        <TabButton onClick={() => handleSelect("components")}>Components</TabButton>
-                        <TabButton onClick={() => handleSelect("JSX")}>JSX</TabButton>
-                        <TabButton onClick={() => handleSelect("Props")}>Props</TabButton>
-                        <TabButton onClick={() => handleSelect("State")}>State</TabButton>
-                    </menu>
-                    {tabContent}
-                </section>
-            </main>
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onClick={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onClick={() => handleSelect("JSX")}>JSX</TabButton>
+            <TabButton onClick={() => handleSelect("Props")}>Props</TabButton>
+            <TabButton onClick={() => handleSelect("State")}>State</TabButton>
+          </menu>
+          {tabContent}
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    allowedHosts: ["3wfk2w-5174.csb.app"],
+  },
 });
