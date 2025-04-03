7373d55b1a5754ad374d4ee34f31e654185985b8
woon <115874237+jiwoon-code@users.noreply.github.com>
2025-04-03 16:19:22 +0000

    dynamic ouputing array

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["3wfk2w-5174.csb.app", "swpfcm-5173.csb.app"],
+    allowedHosts: ["3wfk2w-5174.csb.app", "swpfcm-5173.csb.app", "3glnq2-5173.csb.app"],
   },
 });
